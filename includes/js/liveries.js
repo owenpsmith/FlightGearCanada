d3d6366f8db4810780bfc22d71ec968aa257c8dd
owenpsmith <owenpsmith@shaw.ca>
2015-02-19 13:08:52 -0800

    parse data and indexes WORKING for DC-3
    type and livery json files parsed, aircraft data injected into livery
    data, and indexes created and sorted

--- includes/js/liveries.js
+++ includes/js/liveries.js
@@ -1,18 +1,32 @@
 
 // data
-var types     = [];
-var liveries  = [];
+var types              = [];
+var liveries           = [];
 
 // indexes
-var operators = [];
-var authors   = [];
+var typeIndex     = [];
+var operatorIndex = [];
+var authorIndex   = [];
+
+
+function addModelData(livery) {
+	$.each(types, function(key, val) {
+		if (val.id == livery.typeId)
+		{
+			livery.modelName  = val.modelName;
+			livery.modelPath  = val.modelPath;
+			livery.modelSetup = val.setup;
+			return;
+		}
+	});
+}
 
 function loadData() {
-	alert("load aircraft data");
-	// load aircraft type data
+	// load aircraft data
 	$.getJSON( "data/aircraftData.json", function(typeData) {
 		types = typeData.types;
 		alert("aircraft data loaded: " + types[0].id);
+
 		types.sort(function (a, b) {
 			var varA = a.modelName.toUpperCase();
 			var varB = b.modelName.toUpperCase();
@@ -20,29 +34,43 @@ function loadData() {
 		});
 	});
 
-	alert("load livery data");
 	// load livery data
-	$.getJSON( "data/liveryData.json", function(liveries) {
-		alert("livery data loaded");
+	$.getJSON( "data/liveryData.json", function(liveryData) {
+		liveries = liveryData.liveries;
+		alert("livery data loaded: " + liveries[0].operator);
+
+		// populate type data into livery data
+		$.each(liveries, function(key, val) {
+			addModelData(val);
+		});
+		alert("model data added");
+
 
 		//create indexes
 		$.each(liveries, function(key, val) {
-			if (operators.indexOf(val.operator) == -1) {
-				operators.push(val.operator);
+			if (typeIndex.indexOf(val.modelName) == -1) {
+				typeIndex.push(val.modelName);
 			}
-			if (authors.indexOf(val.author) == -1) {
-				authors.push(val.author);
+			if (operatorIndex.indexOf(val.operator) == -1) {
+				operatorIndex.push(val.operator);
+			}
+			if (authorIndex.indexOf(val.author) == -1) {
+				authorIndex.push(val.author);
 			}
 		});
+		alert("indexes created");
 
 		// sort indexes
-		operators.sort();
-		authors.sort();
+		typeIndex.sort();
+		operatorIndex.sort();
+		authorIndex.sort();
+		alert("indexes sorted");
 	});
 
 //	alert("operators = " + operators);
 //	alert(authors);
 }
+
 
 
 function populateWidgets() {
@@ -85,7 +113,6 @@ function displayAllByType() {
 
 
 $(document).ready( function() {
-	alert("ready");
 
 	loadData();
 
