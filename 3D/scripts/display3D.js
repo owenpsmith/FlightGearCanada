f616e5e735d75bb5505be5e7600ad5132c48d116
owenpsmith <owenpsmith@shaw.ca>
2015-03-04 10:23:27 -0800

    refactor 3D model files

---
+++ 3D/scripts/display3D.js
@@ -0,0 +1,72 @@
+var canvas;
+var details;
+var loading;
+var viewer;
+var current;
+
+function onLoad(){
+  canvas = document.getElementById("canvas");
+  details = document.getElementById("details");
+  loading = document.getElementById("loading");
+  viewer = new HG.Viewer(canvas);
+  current = 0;
+
+  resize();
+  createGallery(Models);
+  showModel(Models[current]);
+};
+
+function resize(){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  window.addEventListener("resize",
+    function(event){
+      viewer.onResize(window.innerWidth, window.innerHeight);
+    }, false);
+};
+
+function createGallery(models){
+  for (var i = 0; i < models.length; ++ i){
+    var img = document.createElement("img");
+
+    img.id = "img" + i;
+    img.className = "thumbnail" + (i === current? " selected": "");
+    img.src = models[i].thumbnail;
+    img.alt = models[i].name;
+    img.title = models[i].name;
+    img.style.left = (5 + 100 * i) + "px";
+    img.onclick = onClick;
+
+    document.body.appendChild(img);
+  }
+};
+
+    function onClick(){
+
+        var selected = parseInt( this.id.substring( this.id.length - 1 ) );
+
+        if (current !== selected){
+
+            var img = document.getElementById("img" + current);
+            img.setAttribute("class", "thumbnail");
+
+            current = selected;
+            this.setAttribute("class", "thumbnail selected");
+
+            showModel(Models[current]);
+        }
+    };
+
+    function showModel(model){
+        loading.style.display = "block";
+
+        details.innerHTML = "<p id='name'>" + model.name + "</p>";
+
+        viewer.show(model.file, model.setup, onLoaded);
+    };
+
+    function onLoaded(){
+        loading.style.display = "none";
+    };
+
