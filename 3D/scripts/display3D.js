97803fe297064660135c9380cecba62ea6e0a5d7
owenpsmith <owenpsmith@shaw.ca>
2015-03-06 11:54:13 -0800

    buggy 3D enhnacements with EXCEPTION

--- 3D/scripts/display3D.js
+++ 3D/scripts/display3D.js
@@ -38,7 +38,7 @@ function createGallery(models)
 		img.src = models[i].thumbnail;
 		img.alt = models[i].name;
 		img.title = models[i].name;
-		img.style.left = (5 + 100 * i) + "px";
+		img.style.left = (5 + 128 * i) + "px";
 		img.onclick = onClick;
 
 		document.body.appendChild(img);
@@ -67,10 +67,13 @@ function showModel(model)
 
 	details.innerHTML = "<p id='name'>" + model.name + "</p>";
 
+console.log("show");
 	viewer.show(model.file, model.setup, onLoaded);
+console.log("showed");
 };
 
 function onLoaded(){
+console.log("loaded");
 	loading.style.display = "none";
 };
 
